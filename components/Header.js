b9ee180d4aa274cfeeefab1f68d5ff58676988d9
william-shelly <52547555+william-shelly@users.noreply.github.com>
2022-01-04 17:06:42 -0600

    Adding link to logo to go to root of site

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,14 @@
+import Link from "next/link"
 import Nav from "./Nav"
 
 export default function Header(props) {
   return (
     <div className="nav-container">
-      <img src="/img/logo.png" alt="nextbnb" />
+      <Link href="/">
+        <a>
+          <img src="/img/logo.png" alt="nextbnb" />
+        </a>
+      </Link>
       <Nav />
     </div>
   )
