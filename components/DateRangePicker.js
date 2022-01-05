5d14f4eeb4cd991cc846c17fe6c7c2fea576c244
william-shelly <52547555+william-shelly@users.noreply.github.com>
2022-01-04 20:10:56 -0600

    Added date formatting and ability to not select dates after selected date

--- components/DateRangePicker.js
+++ components/DateRangePicker.js
@@ -1,38 +1,89 @@
 import DayPickerInput from "react-day-picker/DayPickerInput"
 import "react-day-picker/lib/style.css"
+import dateFnsFormat from 'date-fns/format'
+import dateFnsParse from 'date-fns/parse'
+import { DateUtils } from "react-day-picker"
+import { useState } from "react"
+
+const today = new Date()
+const tomorrow = new Date(today)
+tomorrow.setDate(tomorrow.getDate() + 1)
+
+const numberOfNightsBetweenDates = (startDate, endDate) => {
+  const start = new Date(startDate) // clone
+  const end = new Date(endDate) // clone
+  let dayCount = 0
+
+  while (end > start) {
+    dayCount ++
+    start.setDate(start.getDate() + 1)
+  }
+  return dayCount
+}
+
+const parseDate = (str, format, locale) => {
+  const parsed = dateFnsParse(str, format, new Date(), { locale })
+  return DateUtils.isDate(parsed) ? parsed : null
+}
+
+const formatDate = (date, format, locale) => dateFnsFormat(date, format, {locale})
+
+const format = 'dd MMM yyyy'
 
 export default function DateRangePicker() {
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+
   return (
     <div className="date-range-picker-container">
-      <div>
+      <div className="mb-2 py-2">
         <label className="block">From:</label>
-        <DayPickerInput />
+        <DayPickerInput
+          formatDate={formatDate}
+          format={format}
+          value={startDate}
+          parseDate={parseDate}
+          placeholder={`${dateFnsFormat(new Date(), format)}`}
+          dayPickerProps={{
+            modifiers: {
+              disabled: {
+                before: new Date()
+              }
+            }
+          }}
+          onDayChange={(day) => {
+            setStartDate(day)
+            if (numberOfNightsBetweenDates(day, endDate) < 1) {
+              const newEndDate = new Date(day)
+              newEndDate.setDate(newEndDate.getDate() + 1)
+              setEndDate(newEndDate)
+            }
+          }}
+        />
       </div>
-      <div>
+      <div className="mb-2 py-2">
         <label className="block">To:</label>
-        <DayPickerInput />
+        <DayPickerInput
+          formatDate={formatDate}
+          format={format}
+          value={endDate}
+          parseDate={parseDate}
+          placeholder={`${dateFnsFormat(new Date(), format)}`}
+          dayPickerProps={{
+            modifiers: {
+              disabled: [
+                startDate,
+                {
+                  before: startDate
+                }
+              ]
+            }
+          }}
+          onDayChange={(day) => {
+            setEndDate(day)
+          }}
+        />
       </div>
-      <style jsx>
-        {`
-          .date-range-picker-container div {
-            display: grid;
-            grid-template-columns: 30% 70%;
-            padding: 10px;
-          }
-          label {
-            padding-top: 10px;
-          }
-        `}
-      </style>
-      <style jsx global>
-        {`
-          .DayPickerInput input {
-            width: 120px;
-            padding: 10px;
-            font-size: 16px;
-          }
-        `}
-      </style>
     </div>
   )
 }
